3439b36929b31c26f049f19de3c9e9b6dfc4cfa7
Lã Nam <namla2000k@gmail.com>
2021-09-13 08:48:46 +0700

    alien

---
+++ DemoMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
