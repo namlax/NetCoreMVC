935e42c7a6cff659d4d6cc97b1c7d6cb2594bae1
Lã Nam <namla2000k@gmail.com>
2021-09-22 08:07:26 +0700

    alien

--- DemoMVC/Data/ApplicationContext.cs
+++ DemoMVC/Data/ApplicationContext.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using MvcMovie.Models;
-
-    public class ApplicationContext : DbContext
+using DemoMVC.Models;
+namespace DemoMVC.Data
+{
+        public class ApplicationContext : DbContext
     {
         public ApplicationContext (DbContextOptions<ApplicationContext> options)
             : base(options)
@@ -16,3 +17,5 @@ using MvcMovie.Models;
 
         public DbSet<MvcMovie.Models.Student> Student { get; set; }
     }
+
+}
