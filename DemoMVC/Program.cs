ac6c6139323d8552ca2c94a0d4ec6f4fd85d9d5b
Lã Nam <namla2000k@gmail.com>
2021-09-22 08:15:19 +0700

    alien

--- DemoMVC/Program.cs
+++ DemoMVC/Program.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using DemoMVC.Data;
+using DemoMVC.Models;
+using System;
 
 namespace DemoMVC
 {
