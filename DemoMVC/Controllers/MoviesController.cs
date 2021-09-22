e7d4cd008a59db095c8607325126e6b961ecb6b9
Lã Nam <namla2000k@gmail.com>
2021-09-22 08:21:17 +0700

    alien

--- DemoMVC/Controllers/MoviesController.cs
+++ DemoMVC/Controllers/MoviesController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using MvcMovie.Models;
+using DemoMVC.Models;
 
 namespace DemoMVC.Controllers
 {
